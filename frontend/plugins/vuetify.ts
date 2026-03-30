425fc12bb9f081c0c093f9583c2cfeeca4f55fc2
artemkhrulkov <artemhrulkov@gmail.com>
2026-03-31 01:40:27 +0800

    Fix IconifyComponent import path after moving to iconify folder

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,7 +1,7 @@
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import { IconifyComponent } from "~/components/IconifyComponent";
+import { IconifyComponent } from "~/components/iconify/IconifyComponent";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
